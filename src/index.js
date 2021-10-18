a59876ab8b6af3d1e5871524115bf3aa8df2ec0c
AntonioF <joseantonio1186@hotmail.com>
2021-10-18 13:54:05 -0600

    agregar actualizacion del puerto

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 const http = require('./app');
 
-http.listen(process.env.PORT || 3000, () => {
-    console.log('Servidor en el puerto 3000');
-})
+//http.listen(process.env.PORT || 3000, () => {
+  //  console.log('Servidor en el puerto 3000');
+//})
+
+const PORT = process.env.PORT || 3000;
+http.listen(PORT, () => {
+    console.log(`app corriendo en el puerto ${ PORT }`);
+});
 
 
 //let port = process.env.PORT || 3000;
